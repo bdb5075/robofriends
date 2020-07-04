f8883ef45b00331c89fab35fb34de1665b90f88e
Brittany <bdb5075@gmail.com>
2020-07-04 14:39:12 -0400

    initial commit

---
+++ src/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+	return (
+		<div>
+			{robots.map(robot => 
+				<Card 
+					name = {robot.name} 
+					email = {robot.email} 
+					id = {robot.id}
+				/>
+			)
+		} 
+		</div>
+		);
+	}
+		
+
+export default CardList;
