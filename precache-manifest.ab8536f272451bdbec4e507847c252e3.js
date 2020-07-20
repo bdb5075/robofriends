1f657e6ddd7b2afd06bbd1344c8e40139c159079
Brittany <bdb5075@gmail.com>
2020-07-20 06:30:44 -0400

    Updates

---
+++ precache-manifest.ab8536f272451bdbec4e507847c252e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1168d7d8423d75da2b76576c4b3b0a7b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "422b5641aebcc6c44b90",
+    "url": "/robofriends/static/css/main.3f991fae.chunk.css"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422b5641aebcc6c44b90",
+    "url": "/robofriends/static/js/main.62436cf3.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
